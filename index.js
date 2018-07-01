efed2736a2a8ab3f626c93505ca83ac4190548ec
Abhishek Shukla <in.abhishek.shukla@gmail.com>
2018-07-01 22:55:46 +0530

    Modified indexJS

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-import { AppRegistry,View,Text } from 'react-native';
-import App from './src/App';
-
-// const App = () => {
-//     return (
-//         <View>
-//             {/* <App /> */}
-//             <Text>Hello</Text>
-//         </View>
-//     )
-// }
+import { AppRegistry,View,Text } from 'react-native'
+import App from './src/App'
 
 AppRegistry.registerComponent('auth', () => App);
